71050282ecc9f871c32aabda2816101d2275d3b7
Quin <burrellq@gmail.com>
2021-05-23 13:49:10 +1200

    Added functions that determine bounds, capacities, and demand volumes.

--- main.py
+++ main.py
@@ -3,67 +3,76 @@ Quin Burrell
 Alex McCarty
 """
 
-def demand_volume(X, Y, Z):
+
+def demand_vols(X, Y, Z):
+    """determines the demand volumes on each source to destination pair"""
+    result = []
+    for src in range(1, X+1):
+        for dest in range(1, Z+1):
+            LHS = []
+            for trans in range(1, Z+1):
+                LHS += ['X{}{}{}'.format(src, trans, dest)]
+            result += ['{} = {}'.format(' + '.join(LHS), str(src + dest))]
+    return '\n'.join(result)
 
 
-
-def source2trans_cap(X, Y, Z):
-
-
-
-def trans2dest_cap(X, Y, Z):
-
-
-
-def trans_nodes(X, Y, Z):
-
-
-
-def binary_vars(X, Y, Z):
+def STcapps(X, Y, Z):
+    """determines the link capacity constraints from source to transit nodes"""
+    result = []
+    for src in range(1, X+1):
+        for trans in range(1, Y+1):
+            LHS = []
+            for dest in range(1, Z+1):
+                LHS += ['X{}{}{}'.format(src, trans, dest)]
+            result += ['{} - (C{}{} * r) <= 0'.format(' + '.join(LHS), src, trans)]
+    return '\n'.join(result)
 
 
+def TDcapps(X, Y, Z):
+    """determines the link capacity constraints from transit to destination nodes"""
+    result = []
+    for trans in range(1, Y + 1):
+        for dest in range(1, Z + 1):
+            LHS = []
+            for src in range(1, X + 1):
+                LHS += ['X{}{}{}'.format(src, trans, dest)]
+            result += ['{} - (D{}{} * r) <= 0'.format(' + '.join(LHS), trans, dest)]
+    return '\n'.join(result)
 
-def demand_flow(X, Y, Z):
+
+def capps(X, Y, Z):
+    """returns a string of all capacity constraints"""
+    return '{}\n{}'.format(STcapps(X, Y, Z), TDcapps(X, Y, Z))
 
 
+def bounds(X, Y, Z):
+    """determines the bounds"""
+    result = ['r >= 0']
+    for src in range(1, X+1):
+        for trans in range(1, Y+1):
+            result += ['C{}{} >= 0'.format(src, trans)]
+            for dest in range(1, Z+1):
+                result += ['X{}{}{} >= 0'.format(src, trans, dest)]
+                if src == 1:
+                    result += ['D{}{} >= 0'.format(trans, dest)]
+    return '\n'.join(result)
 
-def bound_vars(X, Y, Z):
 
-
-
-def binaries(X, Y, Z):
-
-
-
-def create_lp(X, Y, Z, N):
+def create_lp(X, Y, Z):
     filename = '{}{}{}.lp'.format(X, Y, Z)
     file = open(filename, 'w')
-    content = '''Minimize r
-    Subject to
-    Demand volume: \n{}
-    Source to tranfer node capacity: \n{}
-    Transit to destination node capacity: \n{}
-    Transit nodes: \n{}
-    Binary variables: \n{}
-    Demand flow: \n{}
-    Bounds: \n{}
-    0 <= r
-    Binaries: \n{}
-    End'''.format(demand_volume(X, Y, Z), source2tras_cap(X, Y, Z),
-    trans2dest_cap(X, Y, Z), trans_nodes(X, Y, Z), binary_vars(X, Y, Z),
-    demand_flow(X, Y, Z), bound_vars(X, Y, Z), binaries(X, Y, Z))
+    content = '''Minimize \nr\nSubject to\nDemand Volumes:\n{}\nCapacity Constraints:\n{}\nBounds: \n{}\nEnd'''\
+        .format(demand_vols(X, Y, Z), capps(X, Y, Z), bounds(X, Y, Z))
 
     file.write(content)
     file.close
 
 
-
 def begin():
-    X = int(input('The amount of source nodes:'))
-    Y = int(input('The amount of transit nodes:'))
-    Z = int(input('The amount of destination nodes:'))
-    N = 3
-    create_lp(X, Y, Z, N)
+    X = int(input('The amount of source nodes: '))
+    Y = int(input('The amount of transit nodes: '))
+    Z = int(input('The amount of destination nodes: '))
+    create_lp(X, Y, Z)
 
 
 begin()
